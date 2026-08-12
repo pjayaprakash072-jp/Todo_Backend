782cb3b469a0afec7d6942fb78870281af642bd4
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-12 20:14:14 +0530

    Add MongoDB connection

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const app = express();
 
 dotenv.config();
 
+app.use(express.json()) // Its job is to read JSON data sent by the client and convert it into a JavaScript object.
+
 const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("MongoDB connected successfully!")
+    })
+    .catch((err)=>{
+        console.log("MongoDB connection error" + err)
+    })
 
 app.get('/home',(req,res)=>{
     res.send("<h1>Welcome to Todo!</h1>")
